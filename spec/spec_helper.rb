5ec35163c191dbb5edb3189f360225529bf75caa
Theo <theo@iconara.net>
2013-08-04 19:15:12 +0200

    Added test coverage initialization code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,22 @@
 # encoding: utf-8
 
 require 'bundler/setup'
+
+unless ENV['COVERAGE'] == 'no'
+  require 'coveralls'
+  require 'simplecov'
+
+  if ENV.include?('TRAVIS')
+    Coveralls.wear!
+    SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  end
+
+  SimpleCov.start do
+    add_group 'Source', 'lib'
+    add_group 'Unit tests', 'spec/etcd'
+    add_group 'Integration tests', 'spec/integration'
+  end
+end
+
 require 'etcd'
 require 'webmock/rspec'
