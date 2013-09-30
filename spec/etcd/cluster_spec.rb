ba79f3f05b2a8ee4c0b8673651364c78280123ac
Roman Heinrich <roman.heinrich@gmail.com>
2013-09-30 23:42:52 +0200

    some refactoring in spec

--- spec/etcd/cluster_spec.rb
+++ spec/etcd/cluster_spec.rb
@@ -6,12 +6,15 @@ module Etcd
 
     include ClusterHelper
 
+    let :cluster_uri do
+      "http://127.0.0.1:4001"
+    end
+
     describe :class_methods do
       describe '#cluster_status' do
         it "returns parsed data" do
-          uri = "http://127.0.0.1:4001"
-          with_stubbed_status(uri) do
-            res = Etcd::Cluster.cluster_status(uri)
+          with_stubbed_status(cluster_uri) do
+            res = Etcd::Cluster.cluster_status(cluster_uri)
             res[0][:name].should == "node1"
           end
         end
@@ -30,18 +33,16 @@ module Etcd
 
       describe '#nodes_from_uri' do
         it "returns node instances created from uri" do
-          uri = "http://127.0.0.1:4001"
-          with_stubbed_status(uri) do
-            nodes = Etcd::Cluster.nodes_from_uri(uri)
+          with_stubbed_status(cluster_uri) do
+            nodes = Etcd::Cluster.nodes_from_uri(cluster_uri)
             nodes.size.should == 3
             nodes.first.class.should == Etcd::Node
           end
         end
 
         it "but those instances have no real status yet" do
-          uri = "http://127.0.0.1:4001"
-          with_stubbed_status(uri) do
-            nodes = Etcd::Cluster.nodes_from_uri(uri)
+          with_stubbed_status(cluster_uri) do
+            nodes = Etcd::Cluster.nodes_from_uri(cluster_uri)
             nodes.size.should == 3
             nodes.first.status == :unknown
           end
@@ -51,10 +52,9 @@ module Etcd
       describe '#init_from_uris', "- preferred way to initialize cluster" do
         describe "in healthy cluster" do
           it "has all nodes at status :running" do
-            uri = "http://127.0.0.1:4001"
-            with_stubbed_status(uri) do
+            with_stubbed_status(cluster_uri) do
               with_stubbed_leaders(healthy_cluster_config) do
-                cluster = Etcd::Cluster.init_from_uris(uri)
+                cluster = Etcd::Cluster.init_from_uris(cluster_uri)
                 nodes = cluster.nodes
                 nodes.size.should == 3
                 nodes.map{|x| x.status}.uniq.should == [:running]
@@ -63,12 +63,11 @@ module Etcd
           end
 
           it "has one leader node" do
-            uri = "http://127.0.0.1:4001"
-            with_stubbed_status(uri) do
+            with_stubbed_status(cluster_uri) do
               with_stubbed_leaders(healthy_cluster_config) do
-                cluster = Etcd::Cluster.init_from_uris(uri)
+                cluster = Etcd::Cluster.init_from_uris(cluster_uri)
                 leader  = cluster.leader
-                leader.etcd.should == uri
+                leader.etcd.should == cluster_uri
               end
             end
           end
@@ -77,10 +76,9 @@ module Etcd
 
         describe "in un-healthy cluster" do
           it "has some  nodes at status :down" do
-            uri = "http://127.0.0.1:4001"
-            with_stubbed_status(uri) do
+            with_stubbed_status(cluster_uri) do
               with_stubbed_leaders(one_down_cluster_config) do
-                cluster = Etcd::Cluster.init_from_uris(uri)
+                cluster = Etcd::Cluster.init_from_uris(cluster_uri)
                 nodes = cluster.nodes
                 nodes.size.should == 3
                 nodes.map{|x| x.status}.uniq.should == [:running, :down]
@@ -93,19 +91,17 @@ module Etcd
 
     describe :instance_methods do
       describe '#new' do
-        it "will not request any info on initilization" do
-          uri = "http://127.0.0.1:4001"
-          cluster = Etcd::Cluster.new(uri)
-          WebMock.should_not have_requested(:get, "http://127.0.0.1:4001/v1/keys/_etcd/machines/?")
+        it "will not request any info on initialization" do
+          cluster = Etcd::Cluster.new(cluster_uri)
+          WebMock.should_not have_requested(:get, "http://127.0.0.1:4001/v1/keys/_etcd/machines/")
         end
       end
 
       describe '#nodes' do
         it "will update nodes info on first nodes access" do
-          uri = "http://127.0.0.1:4001"
-          with_stubbed_status(uri) do
+          with_stubbed_status(cluster_uri) do
             with_stubbed_leaders(healthy_cluster_config) do
-              cluster = Etcd::Cluster.new(uri)
+              cluster = Etcd::Cluster.new(cluster_uri)
               nodes   = cluster.nodes
               nodes.size.should == 3
             end
@@ -113,10 +109,9 @@ module Etcd
         end
 
         it "caches result on further queries" do
-          uri = "http://127.0.0.1:4001"
-          with_stubbed_status(uri) do
+          with_stubbed_status(cluster_uri) do
             with_stubbed_leaders(healthy_cluster_config) do
-              cluster = Etcd::Cluster.new(uri)
+              cluster = Etcd::Cluster.new(cluster_uri)
               nodes   = cluster.nodes
               nodes.map{|x| x.status}.uniq.should == [:running]
               with_stubbed_leaders(one_down_cluster_config) do
@@ -134,10 +129,9 @@ module Etcd
 
       describe '#update_status' do
         it "will re-update node stati from etcd" do
-          uri = "http://127.0.0.1:4001"
-          with_stubbed_status(uri) do
+          with_stubbed_status(cluster_uri) do
             with_stubbed_leaders(healthy_cluster_config) do
-              cluster = Etcd::Cluster.new(uri)
+              cluster = Etcd::Cluster.new(cluster_uri)
               nodes   = cluster.update_status
               nodes.size.should == 3
               nodes.map{|x| x.status}.uniq.should == [:running]
@@ -153,23 +147,21 @@ module Etcd
       describe '#leader' do
 
         it "returns the leader node from nodes" do
-          uri = "http://127.0.0.1:4001"
-          with_stubbed_status(uri) do
+          with_stubbed_status(cluster_uri) do
             with_stubbed_leaders(healthy_cluster_config) do
-              cluster = Etcd::Cluster.new(uri)
+              cluster = Etcd::Cluster.new(cluster_uri)
               leader = cluster.leader
-              leader.etcd.should == "http://127.0.0.1:4001"
+              leader.etcd.should == cluster_uri
               leader.is_leader.should be_true
             end
           end
         end
 
         it "re-sets leader after every update_status" do
-          uri = "http://127.0.0.1:4001"
-          with_stubbed_status(uri) do
+          with_stubbed_status(cluster_uri) do
             with_stubbed_leaders(healthy_cluster_config) do
-              cluster = Etcd::Cluster.new(uri)
-              cluster.leader.etcd.should == "http://127.0.0.1:4001"
+              cluster = Etcd::Cluster.new(cluster_uri)
+              cluster.leader.etcd.should == cluster_uri
               with_stubbed_leaders(healthy_cluster_changed_leader_config) do
                 nodes   = cluster.update_status
                 cluster.leader.etcd.should == "http://127.0.0.1:4002"
