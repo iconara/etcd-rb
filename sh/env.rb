486eb9e04968e4e80b43b25633a4031fdc539ff5
Roman Heinrich <roman.heinrich@gmail.com>
2013-10-11 23:46:27 +0200

    test_client with options

--- sh/env.rb
+++ sh/env.rb
@@ -7,8 +7,9 @@ require 'json'
 require './spec/resources/node_killer'
 
 class Etcd::Client
-  def self.test_client
+  def self.test_client(opts = {})
     seed_uris = ["http://127.0.0.1:4001", "http://127.0.0.1:4002", "http://127.0.0.1:4003"]
-    client = Etcd::Client.connect(:uris => seed_uris)
+    opts.merge!(:uris => seed_uris)
+    client = Etcd::Client.connect(opts)
   end
 end
