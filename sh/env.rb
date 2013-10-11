126c5728386b261e7842c3f00b5e625e710c7e35
Roman Heinrich <roman.heinrich@gmail.com>
2013-10-11 17:36:09 +0200

    test_client method for developer convenience in the terminal, assumes local cluster with 3 nodes

--- sh/env.rb
+++ sh/env.rb
@@ -5,3 +5,10 @@ require 'json'
 
 ## only for testing in the console
 require './spec/resources/node_killer'
+
+class Etcd::Client
+  def self.test_client
+    seed_uris = ["http://127.0.0.1:4001", "http://127.0.0.1:4002", "http://127.0.0.1:4003"]
+    client = Etcd::Client.connect(:uris => seed_uris)
+  end
+end
