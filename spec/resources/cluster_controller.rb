4585dad13ee7e1e794de7cf0a737cd2b7f9378e2
Roman Heinrich <roman.heinrich@gmail.com>
2013-10-12 11:47:02 +0200

    cluster_controller more robust

--- spec/resources/cluster_controller.rb
+++ spec/resources/cluster_controller.rb
@@ -1,14 +1,19 @@
 class ClusterController
+
+  def self.sh_path
+    File.expand_path(File.join(File.dirname(__FILE__), '..', '..', "sh"))
+  end
+
   def self.kill_node(node_name)
     node_pid = `ps -ef|grep #{node_name}|grep -v grep`.split[1]
     `kill -9 #{node_pid}`
   end
 
   def self.start_cluster
-    `sh/cluster start`
+    `#{sh_path}/cluster start`
   end
 
   def self.stop_cluster
-    `sh/cluster stop`
+    `#{sh_path}/cluster stop`
   end
 end
