257d6ab265e936dc2c37c3d6b8e202cf0d0bb5bf
Roman Heinrich <roman.heinrich@gmail.com>
2013-10-12 11:47:42 +0200

    test_client in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,9 +18,17 @@ unless ENV['COVERAGE'] == 'no'
   end
 end
 
-ENV['ETCD_URI'] ||= 'http://127.0.0.1:4001'
-
 require 'webmock/rspec'
 require 'etcd'
 require 'json'
+require './spec/resources/cluster_controller'
+
+class Etcd::Client
+  def self.test_client(opts = {})
+    seed_uris = ["http://127.0.0.1:4001", "http://127.0.0.1:4002", "http://127.0.0.1:4003"]
+    opts.merge!(:uris => seed_uris)
+    client = Etcd::Client.connect(opts)
+  end
+end
+
 Dir["./spec/support/*.rb"].each { |f|  require f}
