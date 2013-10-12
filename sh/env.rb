05415331cd2fee91f521b12ec73ac80bbb6710e8
Roman Heinrich <roman.heinrich@gmail.com>
2013-10-12 11:50:22 +0200

    common_helper for dev utils

--- sh/env.rb
+++ sh/env.rb
@@ -4,12 +4,4 @@ require 'etcd'
 require 'json'
 
 ## only for testing in the console
-require './spec/resources/cluster_controller'
-
-class Etcd::Client
-  def self.test_client(opts = {})
-    seed_uris = ["http://127.0.0.1:4001", "http://127.0.0.1:4002", "http://127.0.0.1:4003"]
-    opts.merge!(:uris => seed_uris)
-    client = Etcd::Client.connect(opts)
-  end
-end
+require './spec/support/common_helper'
---
+++ spec/support/common_helper.rb
@@ -0,0 +1,9 @@
+require './spec/resources/cluster_controller'
+
+class Etcd::Client
+  def self.test_client(opts = {})
+    seed_uris = ["http://127.0.0.1:4001", "http://127.0.0.1:4002", "http://127.0.0.1:4003"]
+    opts.merge!(:uris => seed_uris)
+    client = Etcd::Client.connect(opts)
+  end
+end
