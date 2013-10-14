6f0cfc4fa9e7d7e9acad69e2eaf4c47aec6b6bc0
Roman Heinrich <roman.heinrich@gmail.com>
2013-10-14 09:01:46 +0200

    observer: call handler only if info[:index] has higher value that last index

--- lib/etcd/observer.rb
+++ lib/etcd/observer.rb
@@ -1,5 +1,4 @@
 module Etcd
-  # @private
   class Observer
     include Etcd::Loggable
 
@@ -19,12 +18,18 @@ module Etcd
           @client.watch(@prefix, index: @index) do |value, key, info|
             if @running
               logger.debug "watch fired for #{@prefix} with #{info.inspect} "
-              # next time start watching from next index, if any provided
-              if info[:index]
+
+              ## etcd has a bug: after restart watches with index fire __sometimes__ with all the previous values
+              ## workaround:
+              ##  - execute @handler only if info[:index] had higher value than the last index
+              if info[:index] && @index.to_i <= info[:index]
+                # next time start watching from next index
                 @index = info[:index] + 1
                 logger.debug "index for #{@prefix} ----  #{@index} "
+                @handler.call(value, key, info)
+              else
+                @handler.call(value, key, info) if @index == nil
               end
-              @handler.call(value, key, info)
             end
           end
         end
